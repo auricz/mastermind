1ce51cb42ebc20e662612ae98284b6248ab13e5d
Linker9851 <linker9851@gmail.com>
2020-08-03 17:35:07 -0400

    Add project files.

---
+++ mastermind/mastermind.cpp
@@ -0,0 +1,13 @@
+/* 
+Mastermind is a game where player will guess a code in the form of colors (red, orange, yellow, green, blue, purple).
+The computer will make a code randomly at the start and the player will guess the code.
+After each guess, the computer will give feedback on the guess (if a color is correct but in right or wrong spot).
+Player wins if he/she guesses the code in 10 guesses or less.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
