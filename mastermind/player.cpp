bc36d368a4e2d626d359279f39626e10db568897
Linker9851 <linker9851@gmail.com>
2020-08-05 20:51:56 -0400

    player can now enter guess

--- mastermind/player.cpp
+++ mastermind/player.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include "player.h"
 
+bool Player::is_input_valid(std::string s)
+{
+    if (s.length() == 4)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (s[i] != 'R' && s[i] != 'O' && s[i] != 'Y' && s[i] != 'G' && s[i] != 'B' && s[i] != 'P')
+            {
+                std::cout << "\nOnly enter letters of ROYGBP.\n\n";
+                return false;
+            }
+        }
+        return true;
+    }
+    else if (s.empty())
+    {
+        return false;
+    }
+    else
+    {
+        std::cout << "\nEnter 4 letters only.\n\n";
+        return false;
+    }
+}
+
 std::string Player::ask_player()
 {
     std::string input;
     bool is_valid_input = false;
-    while (!is_valid_input)
+    while (!is_input_valid(input))
     {
         std::cout << "Enter your guess: ";
         std::cin >> input;
--- mastermind/player.h
+++ mastermind/player.h
@@ -3,6 +3,8 @@
 
 class Player
 {
+private:
+	static bool is_input_valid(std::string the_input);
 public:
 	static std::string ask_player();
 };
